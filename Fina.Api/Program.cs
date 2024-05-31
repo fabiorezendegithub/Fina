8f55806a7a29913e2239527476d22a6909327f33
Fabio Rezende <fabio.rezende@gmail.com>
2024-05-31 15:12:14 -0300

    Finalizado dia 2 - Api

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,6 +1,20 @@
+using Fina.Api;
+using Fina.Api.Common.Api;
+using Fina.Api.Endpoint;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+builder.AddDataContexts();
+builder.AddCrossOrigin();
+builder.AddDocumentation();
+builder.AddServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.ConfigureDevEnvironment();
+
+app.UseCors(ApiConfiguration.CorsPolicyName);
+app.MapEndpoints();
 
 app.Run();
